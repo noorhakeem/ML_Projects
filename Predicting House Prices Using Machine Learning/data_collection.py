708bf7034a28db6e8c9038bf8cd6807c75e572c4
Noor Hakeem <153187853+hakeemnoor@users.noreply.github.com>
2024-09-19 07:29:11 +0200

    m

--- Predicting House Prices Using Machine Learning/data_collection.py
+++ Predicting House Prices Using Machine Learning/data_collection.py
@@ -1,17 +1,44 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class Data:
         
     def __init__(self):
-        data = pd.read_csv('test.csv')
+        data = pd.read_csv('house_prices.csv')
         
-        print(data.shape)
-
-
+        # print(f"Dataset Shape(Row, Column): {data.shape}")
 
         # print(data.head())
         # print(data.info())
         # print(data.describe())
+
+        #### Statistic
+        #print(f"Statistic: \n{data.describe()}")
+
+        ### Missing value 
+        # missing_values = data.isnull().sum()
+        # print(missing_values[missing_values > 0])
+
+        ### Catagorical features
+        # print(data['Neighborhood'].unique())
+        # print(data['Neighborhood'].value_counts())
+
+        ###Indentify target variable
+        
+        #print(data['SalePrice'].describe())
+
+        numeric_data = data.select_dtypes(include=[float, int])
+    
+        corr_matrix = numeric_data.corr()
+        # corr_matrix = data.corr()
+
+        # Visualize the correlation matrix with a heatmap
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+        plt.show()
+
+
     
 if __name__ =='__main__':
     Data()
